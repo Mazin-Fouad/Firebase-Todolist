567089e5e9a71918b2541559062d44ac1be3de5b
Mazin Fouad <m.fouad@live.de>
2023-01-29 15:15:51 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Firebase-Todolist';
+export class AppComponent implements OnInit {
+  todos$: Observable<any>;
+  todos: Array<any>;
+  constructor(firestore: Firestore) {
+    const coll = collection(firestore, 'todos');
+    this.todos$ = collectionData(coll);
+
+    this.todos$.subscribe((newTodos) => {
+      console.log('New Todos are:', newTodos);
+      this.todos = newTodos;
+    });
+  }
+
+  ngOnInit() {}
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'mf-todolist-4763d',
+    appId: '1:826863538026:web:47d4339e945b20d489df13',
+    storageBucket: 'mf-todolist-4763d.appspot.com',
+    apiKey: 'AIzaSyAyCIvD4zrFrvJEj_dhMYR2cYWzj2mkyuA',
+    authDomain: 'mf-todolist-4763d.firebaseapp.com',
+    messagingSenderId: '826863538026',
+    measurementId: 'G-JXEEM1DD05',
+  },};
