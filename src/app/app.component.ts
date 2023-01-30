8005c9bef8cd0b5be7446c2e2bf0f6e93fb725ed
Mazin Fouad <m.fouad@live.de>
2023-01-30 15:48:42 +0100

    add data functin to collection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Component } from '@angular/core';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  setDoc,
+  doc,
+} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -7,18 +13,24 @@ import { Observable } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   todos$: Observable<any>;
   todos: Array<any>;
-  constructor(firestore: Firestore) {
+  todoText: string = '';
+
+  constructor(public firestore: Firestore) {
     const coll = collection(firestore, 'todos');
     this.todos$ = collectionData(coll);
 
+    //More elegant way, e.g. shows popup or plays audio when update comes in HTML
     this.todos$.subscribe((newTodos) => {
-      console.log('New Todos are:', newTodos);
+      alert('New Todos Added');
       this.todos = newTodos;
     });
   }
 
-  ngOnInit() {}
+  addTodo() {
+    const coll = collection(this.firestore, 'todos');
+    setDoc(doc(coll), { name: this.todoText });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
